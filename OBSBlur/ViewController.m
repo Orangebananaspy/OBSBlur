4da646cc0e6097f3c6b29098d9d035510b6fc56a
Orangebananaspy <rsanghav12@yahoo.com>
2014-08-27 11:31:00 -0600

    Added blur view

--- OBSBlur/ViewController.m
+++ OBSBlur/ViewController.m
@@ -18,7 +18,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //Adding the the blurview
+    testBlur = [[_UIBackdropView alloc] initWithStyle:2060];
+	[[testBlur inputSettings] setColorTint:[UIColor colorWithRed:0.5 green:0.5 blue:0.0 alpha:1.0]];
+	[[testBlur inputSettings] setColorTintAlpha:0.1];
+    [testBlur setBlurRadiusSetOnce:NO];
+    [testBlur setBlurRadius:4.0];
+    [testBlur setBlurHardEdges:2];
+    [testBlur setBlursWithHardEdges:YES];
+    [testBlur setBlurQuality:@"default"];
+	[self.view addSubview:testBlur];
+    
+    //Hiding it so you can see it animated
+    testBlur.alpha = 0.0f;
 }
+
+
 
 - (void)didReceiveMemoryWarning
 {
